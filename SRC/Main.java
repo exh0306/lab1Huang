61246f75ed2c43700659e37da6044e37f1e7176f
exh0306 <exh5452@psu.edu>
2026-01-22 13:31:34 -0500

    added details

--- SRC/Main.java
+++ SRC/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Motorcycle m2 = new Motorcycle("Pink", Material.STEEL, );
         Motorcycle m3 = new Motorcycle("Blue", Material.TI);
         System.out.println("Motorcycle 1 color: " + m1.getColorA());
-        System.out.println("Motorcycle 1 material: " + m1.getFrameMaterial());
+        System.out.println("Motorcycle 1 material: " + m1.;
         System.out.println("Motorcycle 2 color: " + m2.getColorA());
         System.out.println("Motorcycle 3 color: " + m3.getColorA());
 
